0f84190ea0910389a91d7793743d8ea131af8324
Thrakrien <calebe_sl@hotmail.com>
2024-06-08 00:30:58 -0300

    primeira versão com os executáveis

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pre-configs-linuxzada
-Repositório apenas para automatização dos programas ao fazer distrohopping.
+
+Repositório apenas para automatização dos programas ao fazer distrohopping (a arte de ficar pulando de distro linux igual um maluco).
+
+Entretanto, o arquivo bash serve apenas para distros baseadas em [Debian ou Ubuntu](https://upload.wikimedia.org/wikipedia/commons/a/ad/2023_Linux_Distributions_Timeline.svg), 
+
+Para utilizar o script, talvez seja necessário tornar o mesmo um executável depois de copiar o arquivo *saving_time_install.sh*:
+
+```
+chmod +x saving_time_install.sh
+
+```
+
+E depois, para executar arquivo, basta apenas executarmos o comando abaixo no terminal:
+
+```
+./saving_time_install.sh
+
+```
---
+++ saving_time_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Here go again, iniciando a instalação!"
+
+sudo apt update && sudo apt update -y
+
+echo "Pacotes atualizados com sucesso!"
+
+# basico do basico para codar
+echo "Instando o Git e o Neovim"
+
+sudo apt install -y git nvim
+
+echo "NeoVim instalado com sucesso"
+
+# adicionando chaves e instalando o spotify
+
+echo "Instalando o Spotify (afinal, não tem como trampar sem música)"
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update && sudo apt-get install spotify-client
+
+echo "Instalação concluída com sucesso"
+
+
